475ff0178207e0149337c6972bf405732a413986
Anwesh43 <anweshthecool0@gmail.com>
2021-08-07 16:29:00 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -14,6 +14,7 @@ const colors : Array<string> = [
     "#00C853"
 ] 
 const deg : number = Math.PI / 2
+const strokeFactor : number = 45
 
 class ScaleUtil {
 
@@ -23,5 +24,38 @@ class ScaleUtil {
 
     static divideScale(scale : number, i : number, n : number) : number {
         return Math.min(1 / n, ScaleUtil.maxScale(scale, i, n)) * n 
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawStackLine(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (let j = 0; j < lines; j++) {
+            context.save()
+            context.translate(size / 2 + (w / 2 + size) * ScaleUtil.divideScale(sc2, parts - 1 - j, parts), -context.lineWidth * j)
+            context.rotate(deg * ScaleUtil.divideScale(sc1, 1, 2))
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * ScaleUtil.divideScale(sc1, 0, 2))
+            context.restore()
+        }
+        context.restore()
+    }
+
+    static drawSLNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.fillStyle = colors[i]
+        DrawingUtil.drawStackLine(context, scale)
     }
 }
