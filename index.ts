786fb74903001e3e8507afc6fe95fa6521e51269
Anwesh43 <anweshthecool0@gmail.com>
2021-08-07 17:55:35 +0530

    adding StackLine class

--- index.ts
+++ index.ts
@@ -175,3 +175,26 @@ class SLNode {
         return this 
     }
 }
+
+class StackLine {
+    
+    curr : SLNode = new SLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
