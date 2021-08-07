994f2c77e1cea66d94d9a2ea347c9560d4252851
Anwesh43 <anweshthecool0@gmail.com>
2021-08-07 17:52:37 +0530

    adding SLNode class

--- index.ts
+++ index.ts
@@ -134,3 +134,44 @@ class Animator {
         }
     }
 }
+
+class SLNode {
+
+    prev : SLNode 
+    next : SLNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSLNode(context, this.i, this.state.scale)    
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+    
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SLNode {
+        var curr : SLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
